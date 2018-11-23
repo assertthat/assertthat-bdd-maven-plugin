5f6dffca80137c9c18b7604a60c0d33b58a5de23
Glib Briia <glib.briia@gmail.com>
2018-11-23 09:27:49 +0000

    Update secrets

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_768d285e8175_key -iv $encrypted_768d285e8175_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
+    openssl aes-256-cbc -K $encrypted_3da93b06d8d5_key -iv $encrypted_3da93b06d8d5_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
     gpg --fast-import cd/codesigning.asc
 fi
